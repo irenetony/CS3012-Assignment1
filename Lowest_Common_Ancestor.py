95eca6b8db83d91c7a619f7878ec65168fdfe075
Irene Tony <irenetony@MacBook-Air.local>
2018-09-23 18:57:19 +0100

    main code

---
+++ Lowest_Common_Ancestor.py
@@ -0,0 +1,7 @@
+# A binary tree node
+class Node:
+    # Constructor to create a new binary node
+    def __init__(self, key):
+        self.key =  key
+        self.left = None
+        self.right = None
