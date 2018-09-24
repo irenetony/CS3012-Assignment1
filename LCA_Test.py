5ddaf34044ce2df2c479417cd64f40aa36f41954
Irene Tony <tonyi@tcd.ie>
2018-09-24 23:20:04 +0100

    Testing commits

--- LCA_Test.py
+++ LCA_Test.py
@@ -1,13 +1,5 @@
 import unittest
-# Driver program to test above function
-# Let's create the Binary Tree shown in above diagram
-# root = Node(1)
-# root.left = Node(2)
-# root.right = Node(3)
-# root.left.left = Node(4)
-# root.left.right = Node(5)
-# root.right.left = Node(6)
-# root.right.right = Node(7)
+
 
 class MyTest(unittest.TestCase):
 
@@ -29,4 +21,4 @@ class MyTest(unittest.TestCase):
 
     def nullTest(self):
         # null TestCase
-        self.assertEqual(findLCA(root,))
+        self.assertEqual(findLCA(root,None, None))
